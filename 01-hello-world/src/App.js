bfecd210cbc278a30a4865d70ce3052dbe59791a
sanghaarsha <sanghaarsha@gmail.com>
2021-08-01 21:07:52 +0545

    init

---
+++ 01-hello-world/src/App.js
@@ -0,0 +1,9 @@
+const App = () => {
+    return (
+        <div>
+            <h1>Hello, world!</h1>
+            <h3>This is my first react page!</h3>
+        </div>
+    );
+};
+export default App;
