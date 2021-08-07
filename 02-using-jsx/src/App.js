e428fba5fcdb9823a8f5ce26d7b904ad91b65e30
sanghaarsha <sanghaarsha@gmail.com>
2021-08-07 19:54:22 +0545

    JSX

--- 02-using-jsx/src/App.js
+++
@@ -1,9 +0,0 @@
-const App = () => {
-    return (
-        <div>
-            <h1>Hello, world!</h1>
-            <h3>This is template for React!</h3>
-        </div>
-    );
-};
-export default App;
--- 02-using-jsx/src/index.js
+++ 02-using-jsx/src/index.js
@@ -1,5 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+
+const App = () => {
+    const buttonText = "Click Me!";
+
+    const getText = () => {
+        return "This is returned text!";
+    };
+
+    // const objectVal = { key: "val" };
+
+    return (
+        <div>
+            {/* to avoid conflict with javascript */}
+            {/* for becomes htmlFor, class becomes className*/}
+            <label htmlFor="name" className="label">
+                Enter Name:
+            </label>
+
+            <input type="text" id="name" />
+
+            {/* inline styling : style:{JS-Object} :*/}
+            <button
+                style={{
+                    backgroundColor: "blue",
+                    color: "white",
+                    border: "4px solid pink",
+                    margin: "5px",
+                }}
+            >
+                {/* Using JS Variable */}
+                {buttonText}
+            </button>
+
+            {/* getting value from function */}
+            <div className="getText">{getText()}</div>
+
+            {/* Values JSX Can't Show */}
+            {/* <div className="cantShowObject">{objectVal}</div> */}
+            {/* it will give this error : Error: Objects are not valid as a React child */}
+        </div>
+    );
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
