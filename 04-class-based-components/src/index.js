e17f4fe73f02f623752a8617514cdf6ee3ad9bba
sanghaarsha <sanghaarsha@gmail.com>
2021-08-08 22:19:29 +0545

    creating class based component

--- 04-class-based-components/src/index.js
+++ 04-class-based-components/src/index.js
@@ -1,12 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Season from "./Season";
 
-const App = () => {
-    return (
-        <>
-            <h1>Hello!</h1>
-        </>
-    );
-};
+// Rules for Class Based Components :
+// 1. Must be a JS Class
+// 2. Must extend React.Component
+// 3. Must define a render method that returns some amount of JSX
+
+class App extends React.Component {
+    render() {
+        // Using browser's geolocation API
+        window.navigator.geolocation.getCurrentPosition(
+            (pos) => console.log(pos),
+            (err) => console.log(err)
+        );
+        return (
+            <div>
+                <Season />
+                <div>Latitide :</div>
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
