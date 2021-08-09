45b31806a33c4ebb353ea6856cfff346926825b0
sanghaarsha <sanghaarsha@gmail.com>
2021-08-09 18:58:16 +0545

    lifecycle method

--- 06-lifecycle-methods/src/index.js
+++ 06-lifecycle-methods/src/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// Component lifecycle methods are the functions we can optionally define inside class based components
+// These methods will be automatically called by React at certain points during it's lifecycle
+// LifeCycle : Component Creation -> Show in DOM -> change in state -> also might be removed altogether from DOM!
+// LifeCycle methods are functions that are called during a distinct time at that cycle
+
 class App extends React.Component {
+    // technically constructor() and render() are also lifecycle methods, as they run during a distinct time at component's creation/render
     constructor(props) {
         super(props);
         this.state = { lat: null, errorMessage: "" };
@@ -15,6 +21,18 @@ class App extends React.Component {
             }
         );
     }
+
+    // this lifecycle method runs when a component first shows up
+    componentDidMount() {
+        console.log("Component is rendered!!");
+    }
+    // this lifecycle method runs everytime a component is updated
+    componentDidUpdate() {
+        console.log("Component was just updated!!");
+    }
+
+    // this lifecycle method sits and waits until the component is no longer shown
+    // componentWillUnmount() {}
 
     render() {
         if (this.state.errorMessage && !this.state.lat) {
