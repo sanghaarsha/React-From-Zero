2f928000a4c919d99a055ff611fe4fde581c1047
sanghaarsha <sanghaarsha@gmail.com>
2021-08-09 18:15:15 +0545

    init

--- 06-lifecycle-methods/src/index.js
+++ 06-lifecycle-methods/src/index.js
@@ -1,33 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// Rules of state :
-// Only useable with class components (...hooks lets us use state in functional components)
-// 'State' is a JS Object that contains data relevant to a component
-// Updating 'state' on a component causes the component to (almost) instantly re-render
-// State must be initialized when a component is created
-// State can only be updated using function setState
-
-// Creating Compnent :
-// Our App component is sub-classing React.Component class
-// that means we are borrowing features from that class
 class App extends React.Component {
-    // first method to be called
     constructor(props) {
-        // super is a reference to the parent's constructor function , here: React.Component
-        // this base (parent) class must have its own constructor function too
-        // when we are defining our constructor function inside our App class we are overriding the parents constructor function
-        // so to make sure parent's (React.Component)'s constructor gets called we need super() method
         super(props);
-
-        // initialize the state object
-        // this is the ONLY TIME we direct assign to this.state
         this.state = { lat: null, errorMessage: "" };
 
-        // get position
         window.navigator.geolocation.getCurrentPosition(
             (pos) => {
-                // updating state
                 this.setState({ lat: pos.coords.latitude });
             },
             (err) => {
@@ -36,7 +16,6 @@ class App extends React.Component {
         );
     }
 
-    // React says each component requires "render" method !!
     render() {
         if (this.state.errorMessage && !this.state.lat) {
             return <h1> Error : {this.state.errorMessage} </h1>;
