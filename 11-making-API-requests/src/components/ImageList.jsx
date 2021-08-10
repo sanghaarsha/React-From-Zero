d9042808255f5303b46c62ae0dca996b9d52d863
sanghaarsha <sanghaarsha@gmail.com>
2021-08-10 16:18:45 +0545

    init

---
+++ 11-making-API-requests/src/components/ImageList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+class ImageList extends React.Component {
+    render() {
+        return (
+            <>
+                <h1>Images</h1>
+            </>
+        );
+    }
+}
+
+export default ImageList;
