c91bc7ee42a8b6ea6098878fb3879713d8eed551
sanghaarsha <sanghaarsha@gmail.com>
2021-08-14 10:13:27 +0545

    removed unused var

--- 15-understanding-hooks/src/components/Search.jsx
+++ 15-understanding-hooks/src/components/Search.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-let n = 0;
 const Search = () => {
     const [term, setTerm] = useState("hello world");
     const [results, setResults] = useState([]);
