f576d9c24cebc9c071cd34adb1501d69d43d95aa
sanghaarsha <sanghaarsha@gmail.com>
2021-08-14 08:25:31 +0545

    useEffect Hook Intro

--- 15-understanding-hooks/src/components/Search.jsx
+++ 15-understanding-hooks/src/components/Search.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const url =
-    "https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=";
+    "https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=javascript";
 
 const Search = () => {
     const [term, setTerm] = useState("");
@@ -9,6 +10,12 @@ const Search = () => {
     const onTermChange = (e) => {
         setTerm(e.target.value);
     };
+
+    console.log("I run with every render");
+
+    useEffect(() => {
+        console.log("I only run ONCE!!");
+    }, []);
 
     return (
         <div>
@@ -29,3 +36,27 @@ const Search = () => {
 };
 
 export default Search;
+
+// ? The useEffect Hook :
+
+// *Functional components do not have naturally access to life-cycle methods like : componentDidMount...
+// *useEffect allows functional components to access something similar to life cycle methods [note: not same but similar]
+// *useEffect takes some configuration that tells when a code is needed to be run automatically
+
+// ?three ways of configuration :
+// * 1. only for the first time rendering of component
+// * 2. first time rendering and whenever it re-renders
+// * 3. first time rendering and (whenever it re-renders and when some piece of data is changed)
+
+// ? syntax
+// useEffect(() => {
+//     console.log("hey hey it's executed!");
+// }, []);
+// * first argument is a function that executes when it matches criteria provided by second criteria
+//* second criteria can be nothing, empty array or array with some parameters
+
+// ? meaning of second arguments
+//    []   : Run at inital render
+// nothing : Run at initial render -> run after every re-render
+// [data]  : Run at inital render -> Run after every re-render 'if'
+//           data has changed since last render
