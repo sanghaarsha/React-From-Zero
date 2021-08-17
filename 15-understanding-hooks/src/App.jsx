e0e2552e6a7a5c0ecd1214e581fc2ca499390d8f
sanghaarsha <sanghaarsha@gmail.com>
2021-08-17 20:35:38 +0545

    start translate

--- 15-understanding-hooks/src/App.jsx
+++ 15-understanding-hooks/src/App.jsx
@@ -1,30 +1,65 @@
-import React from "react";
-import Accordion from "./components/Accordion";
-import Search from "./components/Search";
-import Dropdown from "./components/Dropdown";
+import React, { useState } from "react";
+// import Accordion from "./components/Accordion";
+// import Search from "./components/Search";
+// import Dropdown from "./components/Dropdown";
+import Translate from "./components/Translate";
 
-const items = [
-    {
-        title: "What is react?",
-        content: "React is a front end JS framework!",
-    },
-    {
-        title: "Why use react?",
-        content: "React is a fovorite JS library among engineers",
-    },
-    {
-        title: "How do you use react?",
-        content: "React can be used by creating components.",
-    },
-];
+// const items = [
+//     {
+//         title: "What is react?",
+//         content: "React is a front end JS framework!",
+//     },
+//     {
+//         title: "Why use react?",
+//         content: "React is a fovorite JS library among engineers",
+//     },
+//     {
+//         title: "How do you use react?",
+//         content: "React can be used by creating components.",
+//     },
+// ];
+
+// const options = [
+//     {
+//         label: "Red Color",
+//         value: "red",
+//     },
+//     {
+//         label: "Green Color",
+//         value: "green",
+//     },
+//     {
+//         label: "Blue Color",
+//         value: "blue",
+//     },
+// ];
 
 const App = () => {
+    // const [selectedColor, setSelectedColor] = useState(options[0]);
+    // const [showDropdown, setShowDropdown] = useState(true);
+
     return (
-        <div>
-            {/* <Accordion items={items} /> */}
-            {/* <Search /> */}
-            <Dropdown />
-        </div>
+        // <div className="ui container">
+        //     {/* <Accordion items={items} /> */}
+        //     {/* <Search /> */}
+        //     <button
+        //         onClick={() => {
+        //             setShowDropdown(!showDropdown);
+        //         }}
+        //     >
+        //         Toogle Dropdown!
+        //     </button>
+
+        //     {showDropdown ? (
+        //         <Dropdown
+        //             options={options}
+        //             selected={selectedColor}
+        //             onSelectionChange={setSelectedColor}
+        //         />
+        //     ) : null}
+        // </div>
+
+        <Translate />
     );
 };
 
---
+++ 15-understanding-hooks/src/components/Translate.jsx
@@ -0,0 +1,9 @@
+const Translate = (props) => {
+    return (
+        <div className="ui container">
+            <h1>Translate</h1>
+        </div>
+    );
+};
+
+export default Translate;
